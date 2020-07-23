d773f1b93f18132bfb3d2bb0bea576085b5509b5
Galo Sandoval <galosan92@gmail.com>
2020-07-22 17:01:30 -0700

    almost done with mvp

--- gs/src/App.js
+++ gs/src/App.js
@@ -1,8 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios'
+import * as yup from 'yup'
 import logo from './logo.svg';
 import './App.css';
 import Form from './Form'
+import Member from './Member'
+import formSchema from './formSchema';
 
 const initialMembers = [];
 
@@ -12,7 +15,7 @@ const initialFormValues = {
   username: '',
   email: '',
   password: '',
-  terms: false,
+  terms: '',
 }
 
 const initialFormErrors = {
@@ -25,40 +28,110 @@ const initialFormErrors = {
 
 
 function App() {
-  const [ members, setMembers ] = useState(initialMembers)
-  const [ formValues, setFormValues ] = useState(initialFormValues)
-  const [ formErrors, setFormErrors ] = useState(initialFormErrors)
-  const [ disabled, setDisabled ] = useState(initialDisabled)
+  const [members, setMembers] = useState(initialMembers)
+  const [formValues, setFormValues] = useState(initialFormValues)
+  const [formErrors, setFormErrors] = useState(initialFormErrors)
+  const [disabled, setDisabled] = useState(initialDisabled)
 
   const getMembers = () => {
     axios.get('https://reqres.in/api/users')
       .then(res => {
-        setMembers(res.data)
+        setMembers(res.data.data)
+
+        console.log(res.data.data)
       })
       .catch(err => {
         console.log(err, 'uh oh')
       })
   }
 
-  
+  const postNewMember = (newMember) => {
+    axios.post('https://reqres.in/api/users', newMember)
+      .then(res => {
+        setMembers(res.data.data, ...members)
+        setFormValues(initialFormValues)
+      })
+      .catch(err => {
+        debugger
+      })
+  }
 
-  
+  const inputChange = (name, value) => {
+    yup
+      .reach(formSchema, name)
+      .validate(value)
+      .then(res => {
+        setFormErrors({
+          ...formErrors,
+          [name]: "",
+        })
+      })
+      .catch(err => {
+        setFormErrors({
+          ...formErrors,
+          [name]: err.errors[0]
+        })
+      })
+
+    setFormValues({
+      ...formValues,
+      [name]: value
+    })
+  }
+
+  const checkBoxChange = (name, isChecked) => {
+    setFormValues({
+      ...formValues.terms,
+      [name]: isChecked,
+    })
+  }
+
+  const submit = () => {
+    const newMember = {
+      username: formValues.username.trim(),
+      email: formValues.email.trim(),
+      password: formValues.password.trim(),
+      terms: formValues.terms,
+    }
+    postNewMember(newMember)
+  }
+  // SIDE EFFECTS
+  useEffect(() => {
+    getMembers()
+  }, [])
+
+  useEffect(() => {
+    formSchema.isValid(formValues).then(valid => {
+      setDisabled(!valid)
+    })
+  }, [formValues])
+
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* {members.map(member => {
+          return (
+            <Member key={member.id} details={member} />
+          )
+        })
+        } */}
+        <Form
+          values={formValues}
+          submit={submit}
+          inputChange={inputChange}
+          checkboxChange={checkBoxChange}
+          disabled={disabled}
+          errors={formErrors}
+        />
+        {
+          members.map(member => {
+            return (
+              <Member key={member.id} details={member} />
+            )
+          })
+        }
       </header>
     </div>
   );
